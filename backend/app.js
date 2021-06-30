89c78d7fa0700ad8d302b21132d19dbb10f7752c
Adam Lovett <adamlovett@alumni.usc.edu>
2021-06-30 16:37:52 -0700

    fix heroku for real

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { environment } = require('./config');
 const { ValidationError } = require('sequelize');
 
 const routes = require('./routes');
